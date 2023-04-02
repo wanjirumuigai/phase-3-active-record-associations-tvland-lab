613d7b847f35902db57ac910b08887e6c4bcb76d
Pauline Muigai <wanjirumuigai@gmail.com>
2023-04-02 10:10:56 +0300

    completed lab

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+  def shows
+    Show.where(network_id: id)
+  end
+
+  def sorry
+    "We're sorry about passing on John Mulaney's pilot"
+  end
 end
